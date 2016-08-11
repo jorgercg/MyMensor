96284122261069bec1a6816a78266aaf874db7e3
Jorge Goncalves <jrcgonc@gmail.com>
2016-08-11 17:31:14 +0100

    Primeiro

--- mymensor/admin.py
+++ mymensor/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from mymensor.models import Photo
+
+class PhotoAdmin(admin.ModelAdmin):
+    model = Photo
+    list_display = ('photoMillisSinceEpoch', 'photoVpNumber', 'photoAssetOwnerNumber', 'photoAssetNumber', 'photoStorageURL', 'photoImageLatitude', 'photoImageLongitude', 'photoDBTimeStamp', 'photoTimeStamp', 'photoProcessed',)
+
+
 # Register your models here.
+
+admin.site.register(Photo, PhotoAdmin)
--- mymensor/models.py
+++ mymensor/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Photo(models.Model):
+    photoMillisSinceEpoch = models.BigIntegerField()
+    photoVpNumber = models.IntegerField()
+    photoAssetOwnerNumber = models.IntegerField()
+    photoAssetNumber = models.IntegerField()
+    photoStorageURL = models.CharField(max_length=255)
+    photoImageLatitude = models.FloatField()
+    photoImageLongitude = models.FloatField()
+    photoDBTimeStamp = models.DateTimeField(auto_now=True)
+    photoTimeStamp = models.DateTimeField(auto_now=False)
+    photoProcessed = models.NullBooleanField()
+
+
