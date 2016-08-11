a1bb42d6fbd72ca3bb77574114cfd9acd4a27909
Jorge Goncalves <jrcgonc@gmail.com>
2016-08-11 17:45:41 +0100

    Segundo

--- mymensor/views.py
+++ mymensor/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from mymensor.models import Photo
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    photos = Photo.objects.all()
+    return render(request, 'index.html', { 'photos': photos,})
