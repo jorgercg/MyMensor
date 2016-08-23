32d773f70397023d75ef0a63e333899725167915
Jorge Goncalves <jrcgonc@gmail.com>
2016-08-24 00:07:48 +0100

    commit 24ago2016 00h07min

--- mymensor/views.py
+++ mymensor/views.py
@@ -1,24 +1,27 @@
 from django.shortcuts import render
 from mymensor.models import Photo, AssetOwner, Asset, Dci, Vp, Tag, MyMensorConfiguration, MyMensorConfigurationForm
 
+
 # Portfolio View
 def portfolio(request):
     photos = Photo.objects.all()
-    return render(request, 'index.html', { 'photos': photos,})
+    return render(request, 'index.html', {'photos': photos,})
+
 
 # Photo Feed View
 def photofeed(request):
     photos = Photo.objects.all()
-    return render(request, 'photofeed.html', { 'photos': photos,})
+    return render(request, 'photofeed.html', {'photos': photos,})
+
 
 # Setup View
 def myMensorSetupSideFormView(request):
     if request.method == 'POST':
         form = MyMensorConfigurationForm(request.POST)
-        #if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
+        # if form.is_valid():
+        # process the data in form.cleaned_data as required
+        # ...
+        # redirect to a new URL:
     else:
         form = MyMensorConfigurationForm()
-    return render(request, 'setup.html', { 'form': form,})
+    return render(request, 'setup.html', {'form': form,})
