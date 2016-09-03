be951fc5174d4c6ecd9f2c82aa3b23c33ebfc80b
Jorge Goncalves <jrcgonc@gmail.com>
2016-09-03 13:58:37 +0100

    commit 03sep2016 13h58

--- mymensor/admin.py
+++ mymensor/admin.py
@@ -1,36 +1,36 @@
 from django.contrib import admin
 
-from mymensor.models import AssetOwner, Asset, Dci, Vp, Photo, Tag
-
-class AssetOwnerAdmin(admin.ModelAdmin):
-    model = AssetOwner
-    list_display = ('assetOwnerNumber', 'assetOwnerIsActive',)
+from mymensor.models import Asset, Vp, Photo, Tag, ProcessedTag, Value
 
 class AssetAdmin(admin.ModelAdmin):
     model = Asset
-    list_display = ('assetOwner', 'assetNumber', 'assetIsActive',)
-
-class DciAdmin(admin.ModelAdmin):
-    model = Dci
-    list_display = ('asset', 'dciNumber', 'dciIsActive', 'dciFrequencyUnit', 'dciFrequencyValue', 'dciTolerancePosition', 'dciToleranceRotation')
+    list_display = ('assetDescription', 'assetOwnerDescription', 'assetNumber', 'assetIsActive',)
 
 class VpAdmin(admin.ModelAdmin):
     model = Vp
-    list_display = ('dci', 'vpNumber', 'vpIsActive', 'vpDescription', 'vpStdPhotoStorageURL',)
+    list_display = ('asset', 'vpNumber', 'vpIsActive', 'vpDescription', 'vpStdPhotoStorageURL',)
 
-class PhotoAdmin(admin.ModelAdmin):
-    model = Photo
-    list_display = ('vp', 'photoMillisSinceEpoch', 'photoVpNumber', 'photoAssetOwnerNumber', 'photoAssetNumber', 'photoStorageURL', 'photoImageLatitude', 'photoImageLongitude', 'photoDBTimeStamp', 'photoTimeStamp', 'photoProcessed',)
-    
 class TagAdmin(admin.ModelAdmin):
     model = Tag
     list_display = ('vp', 'tagNumber', 'tagIsActive', 'tagListNumber', 'tagDescription' )    
-    
-    
+
+class PhotoAdmin(admin.ModelAdmin):
+    model = Photo
+    list_display = ('vp', 'photoMillisSinceEpoch', 'photoVpNumber', 'photoAssetNumber', 'photoStorageURL', 'photoImageLatitude', 'photoImageLongitude', 'photoDBTimeStamp', 'photoTimeStamp', 'photoProcessed' )
+
+class ProcesedTagAdmin(admin.ModelAdmin):
+    model = ProcessedTag
+    list_display = ( 'photo', 'tag', 'valValueEvaluated', 'valValueEvaluatedEntryDBTimeStamp', 'tagStateEvaluated' )
+
+class ValueAdmin(admin.ModelAdmin):
+    model = Value
+    list_display = ('processedTag', 'processorUserId', 'valValue', 'valValueEntryDBTimeStamp', 'valEvalStatus', 'tagStateResultingFromValValueStatus')
+
+
 # Register your models here.
-admin.site.register(AssetOwner, AssetOwnerAdmin)
 admin.site.register(Asset, AssetAdmin)
-admin.site.register(Dci, DciAdmin)
 admin.site.register(Vp, VpAdmin)
-admin.site.register(Photo, PhotoAdmin)
 admin.site.register(Tag, TagAdmin)
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(ProcessedTag, ProcesedTagAdmin)
+admin.site.register(Value, ValueAdmin)
