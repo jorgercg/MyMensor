7825db31213e24800afd99bd6456a0b1ddf5a694
Jorge Goncalves <jrcgonc@gmail.com>
2016-09-07 21:09:47 +0100

    commit 07set2016 21h09min

--- mymensor/models.py
+++ mymensor/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 from django.conf import settings
 from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import User
+from allauth.account.models import EmailAddress
 
 
 @python_2_unicode_compatible
@@ -126,3 +128,21 @@ class Value(models.Model):
     valValueEntryDBTimeStamp = models.DateTimeField(auto_now_add=True)
     valEvalStatus = models.CharField(max_length=50, null=True)
     tagStateResultingFromValValueStatus = models.IntegerField()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+
+    def __str__(self):
+        return "{}'s profile".format(self.user.username)
+
+    class Meta:
+        db_table = 'user_profile'
+
+    def account_verified(self):
+        if self.user.is_authenticated:
+            result = EmailAddress.objects.filter(email=self.user.email)
+            if len(result):
+                return result[0].verified
+        return False
+
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
