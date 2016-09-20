e936618e43e0324c93a898939c4f1d9e3f34b688
Jorge Goncalves <jrcgonc@gmail.com>
2016-09-20 08:55:24 +0100

    commit 20set2016 08h55min

--- mymensorapp/settings_production.py
+++ mymensorapp/settings_production.py
@@ -5,8 +5,10 @@ DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+SECURE_SSL_REDIRECT = True
+
 # UPDATE BEFORE LAUNCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['app.mymensor.com']
 
 DEBUG = True
 
