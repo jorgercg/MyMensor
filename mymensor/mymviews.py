2c2e422c759ad8bdb7064a43ef295539576e0309
Jorge Goncalves <jrcgonc@gmail.com>
2016-10-07 19:21:34 +0100

    07oct2016 19h21min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -43,9 +43,7 @@ def cognitoauth(request):
     if request.method == "GET":
         client = boto3.client(
             'cognito-identity',
-            region='eu-west-1',
-            aws_access_key_id='AKIAI4HUWKFMXTSLG5JA',
-            aws_secret_access_key='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',)
+            )
         response = client.get_open_id_token_for_developer_identity(
             IdentityPoolId='eu-west-1:963bc158-d9dd-4ae2-8279-b5a8b1524f73',
             IdentityId='eu-west-1:pacatatucotianao',
