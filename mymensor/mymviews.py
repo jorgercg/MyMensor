1ba67660c030526bc73d5e01d2c8f4b99054ae87
Jorge Goncalves <jrcgonc@gmail.com>
2016-10-14 15:47:12 +0100

    14oct2016 15h47min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -44,21 +44,24 @@ def cognitoauth(request):
         client = boto3.client(
             'cognito-identity',
             'eu-west-1',
-            aws_access_key_id = 'AKIAI4HUWKFMXTSLG5JA',
+            aws_access_key_id = 'AKIAI4HUWKFMXTrequest.authSLG5JA',
             aws_secret_access_key = '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',
             )
-
+        token = request.auth
+        user = request.user
+        email = user.email
         response = client.get_open_id_token_for_developer_identity(
             IdentityPoolId='eu-west-1:963bc158-d9dd-4ae2-8279-b5a8b1524f73',
             IdentityId='eu-west-1:750809e0-cc0e-47d8-bab0-659bd6b55424',
             Logins={
-                'cogdevserv.mymensor.com': 'jrcgonc@gmail.com'
+                'cogdevserv.mymensor.com': email
             },
             TokenDuration=600
         )
         response.update({'identityPoolId':'eu-west-1:963bc158-d9dd-4ae2-8279-b5a8b1524f73'})
         response.update({'identityId':'eu-west-1:750809e0-cc0e-47d8-bab0-659bd6b55424'})
-        response.update({'key': '07cda8a18180252862884d7c748faf8bb5c0cb89'})
+
+        response.update({'key': token})
         return JsonResponse(response)
     return HttpResponse(status=400)
 
