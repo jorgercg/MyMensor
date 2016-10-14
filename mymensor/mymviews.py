a0877d29222c36a1a493701940da1f92252fed37
Jorge Goncalves <jrcgonc@gmail.com>
2016-10-14 19:20:40 +0100

    14oct2016 19h20min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -63,9 +63,7 @@ def cognitoauth(request):
             TokenDuration=600
         )
         response.update({'identityPoolId':'eu-west-1:963bc158-d9dd-4ae2-8279-b5a8b1524f73'})
-        response.update({'key': '07cda8a18180252862884d7c748faf8bb5c0cb89'})
-        response.update({'email': email })
-        response.update({'ntoken' : token })
+        response.update({'key': token})
         return JsonResponse(response)
     return HttpResponse(status=400)
 
