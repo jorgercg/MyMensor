c17b938872f67a7c6ef7f8e1dd552fb354dbc191
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-05 22:59:09 +0000

    05jan2017 22h14min

--- mymensor/serializer.py
+++ mymensor/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import AmazonSNSNotification, AmazonS3Message
 
 class AmazonSNSNotificationSerializer(serializers.ModelSerializer):
-    Message = serializers.CharField(source="key")
+    Message = serializers.CharField(source="Message")
 
     class Meta:
         model = AmazonSNSNotification
