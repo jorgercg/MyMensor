10dfc24b25fcba53e16703275ee2604d416c20db
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-05 21:11:35 +0000

    05jan2017 21h11min

--- mymensor/models.py
+++ mymensor/models.py
@@ -177,6 +177,6 @@ def save_s3_message(sender, instance=None, created=False, **kwargs):
     if created:
         s3message = json.loads(AmazonSNSNotification.Message)
         amzs3msg = AmazonS3Message()
-        amzs3msg.s3_object_key = s3message['Records']['s3']['object']['key']
+        amzs3msg.s3_object_key = s3message.get('Records.s3.object.key', None)
         amzs3msg.save()
 
