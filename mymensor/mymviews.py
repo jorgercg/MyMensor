42dfb871f6cef9100d0133105f88d024abfe54bf
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-06 14:48:57 +0000

    06jan2017 14h46min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -23,7 +23,7 @@ def amazon_sns_processor(request):
         if serializer.is_valid():
             serializer.save()
             amzs3msg = AmazonS3Message()
-            amzs3msg.eventVersion = body_initial[0][4][0][0] #eventVersion
+            amzs3msg.eventVersion = body_initial[0][1][0][0] #eventVersion
             amzs3msg.save()
             return HttpResponse(status=200)
     return HttpResponse(status=400)
