fc085078cccaf42ef1d8a71a17abbbc3807c511d
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-11 19:06:13 +0000

    11jan2017 17h56min

--- mymensor/static/js/mymensor-jquery.js
+++ mymensor/static/js/mymensor-jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 // JQuery code to be added in here.
     var url = window.location;
     // Will only work if string in href matches with location
-        $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+        $('ul.item').parent().addClass('active');
 
     // Will also work for relative and absolute hrefs
         $('ul.nav a').filter(function () {
