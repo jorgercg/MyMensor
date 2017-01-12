6b9b3d7a1ebc6cc6ea2cd8c9be22562b7ccb4a30
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-12 09:53:04 +0000

    12jan2017 09h12min

--- mymensor/static/js/mymensor-jquery.js
+++ mymensor/static/js/mymensor-jquery.js
@@ -1,6 +1,4 @@
-$(document).ready(function() {
-    $(".nav a").on("click", function(){
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+$(".nav a").on("click", function(){
+     $(".nav").find(".active").removeClass("active");
+     $(this).parent().addClass("active");
 });
