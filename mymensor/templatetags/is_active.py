d7e6c0a3b4a272a05cb5a6f06d14d10468f9b83a
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-12 18:05:41 +0000

    12jan2017 18h05min

---
+++ mymensor/templatetags/is_active.py
@@ -0,0 +1,11 @@
+## is_active.py
+from django.template import Library
+from django.core.urlresolvers import reverse
+register = Library()
+
+@register.simple_tag
+def is_active(request, url):
+    # Main idea is to check if the url and the current path is a match
+    if request.path in reverse(url):
+        return "active"
+    return
