f53f915d8df0830c260f61a46d4e879b457ef7eb
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-12 09:30:51 +0000

    12jan2017 09h12min

--- mymensor/static/js/mymensor-jquery.js
+++ mymensor/static/js/mymensor-jquery.js
@@ -1,6 +1,5 @@
-$(document).ready(function() {
-    $(".nav a").on("click", function(){
-       $(".nav").find(".active").removeClass("active");
-       $(this).addClass("active");
-    });
+// Activate menu item on click
+$(".nav a").on("click", function(){
+   $(".nav").find(".active").removeClass("active");
+   $(this).addClass("active");
 });
