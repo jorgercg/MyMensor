31722a1955a60cf21615f08e0d715369dc60fbd6
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-13 01:23:00 +0000

    12jan2017 18h05min

--- staticfiles/js/mymensor-jquery.js
+++ staticfiles/js/mymensor-jquery.js
@@ -5,6 +5,8 @@ $('#mediaModal').on('show.bs.modal', function (event) {
         var recipient = button.data('whatever') // Extract info from data-* attributes
         // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
         // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+            alert(recipient)
+
         var modal = $(this)
         modal.find('#mediaContent').val(src=recipient)
     })
