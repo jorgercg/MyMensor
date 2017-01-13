d875797785b14c593c5bc8fc3265abd177c33fd6
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-13 17:00:59 +0000

    13jan2017 17h00min

--- mymensor/app.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyMensorConfig(AppConfig):
-    name = 'mymensor'
-
-    def ready(self):
-        import mymensor.signals
--- mymensor/apps.py
+++ mymensor/apps.py
@@ -5,3 +5,6 @@ from django.apps import AppConfig
 
 class MymensorConfig(AppConfig):
     name = 'mymensor'
+
+    def ready(self):
+        import mymensor.signals
