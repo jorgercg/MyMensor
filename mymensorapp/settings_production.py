3c89c5abc22f363fc4b895c5055999218713cad3
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-16 12:49:36 +0000

    16jan2017 09h21min

--- mymensorapp/settings_production.py
+++ mymensorapp/settings_production.py
@@ -6,8 +6,10 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 
 # UPDATE BEFORE LAUNCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ALLOWED_HOSTS = ['app.mymensor.com', 'mymensor.herokuapp.com']
