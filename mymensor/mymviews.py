308a1cf13b7e63ab7911074403e484ad454a17ad
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-19 10:28:45 +0000

    19jan2017 09h19min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -101,7 +101,10 @@ def portfolio(request):
                                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
         s3Client = session.client('s3')
         vps = Vp.objects.filter(asset__assetOwner=request.user).order_by('vpNumber')
-        medias = Media.objects.filter(vp__asset__assetOwner=request.user).order_by('-mediaTimeStamp').order_by('mediaVpNumber')[:10].filter()
+        allmedias = Media.objects.filter(vp__asset__assetOwner=request.user).order_by('-mediaTimeStamp').order_by('mediaVpNumber')
+        medias = Media()
+        for vp in vps:
+            medias = allmedias.objects.filter(vp=vp)[:10]
         #qtyvps = Asset.objects.get(assetOwner=request.user).assetDciQtyVps
         for media in medias:
             media.mediaStorageURL = s3Client.generate_presigned_url('get_object',
