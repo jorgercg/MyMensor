7103f815dc61cf961732c7b104b73ce05a6d0549
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-20 16:15:03 +0000

    20jan2017 16h11min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -113,7 +113,7 @@ def portfolio(request):
             media.mediaStorageURL = s3Client.generate_presigned_url('get_object',
                                     Params={'Bucket': AWS_S3_BUCKET_NAME,'Key': media.mediaObjectS3Key},
                                     ExpiresIn=3600)
-        return render(request, 'index.html', {'medias': medias, 'vps': vps, 'startdate':startdate, 'enddate':new_enddate})
+        return render(request, 'index.html', {'medias': medias, 'vps': vps, 'startdate':startdate, 'enddate':new_enddate}, status=200)
 
 
 # Media Feed View
