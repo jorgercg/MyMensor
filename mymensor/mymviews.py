ed416a695497a4a4703d94f34505765841270252
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-20 15:13:46 +0000

    20jan2017 15h08min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -12,7 +12,7 @@ from instant.producers import broadcast
 from mymensor.models import Asset, Vp, Media, AmazonS3Message, AmazonSNSNotification
 from mymensor.serializer import AmazonSNSNotificationSerializer
 from mymensorapp.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME, AWS_DEFAULT_REGION
-import json, boto3, datetime
+import json, boto3
 from datetime import datetime
 #from mymensor.forms import AssetOwnerConfigurationFormSet, AssetConfigurationFormSet, DciConfigurationFormSet
 
@@ -101,8 +101,8 @@ def portfolio(request):
         session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
         s3Client = session.client('s3')
-        startdate = datetime.datetime.strptime(request.GET.get('startdate',(datetime.date.today()-datetime.timedelta(days=30))), '%Y-%m-%d')
-        enddate = datetime.datetime.strptime(request.GET.get('enddate',datetime.date.today()), '%Y-%m-%d')
+        startdate = datetime.strptime(request.GET.get('startdate',(datetime.date.today()-datetime.timedelta(days=30))), '%Y-%m-%d')
+        enddate = datetime.strptime(request.GET.get('enddate',datetime.date.today()), '%Y-%m-%d')
         new_enddate = enddate + datetime.timedelta(days=1)
         vps = Vp.objects.filter(asset__assetOwner=request.user).order_by('vpNumber')
         medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaTimeStamp__range=[startdate,new_enddate]).order_by('-mediaTimeStamp').order_by('mediaVpNumber')
