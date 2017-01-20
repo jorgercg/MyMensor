a9156bf12328e1265f16fb0299d5c32586a4ef76
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-20 15:48:58 +0000

    20jan2017 15h08min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -104,8 +104,8 @@ def portfolio(request):
         session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                                         aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
         s3Client = session.client('s3')
-        startdate = dateutil.parser.parse(request.GET.get('startdate',(datetime.today()-dateutil.relativedelta.relativedelta(months=-1))), '%Y-%m-%d')
-        enddate = dateutil.parser.parse(request.GET.get('enddate',datetime.today()), '%Y-%m-%d')
+        startdate = datetime.strptime(request.GET.get('startdate',(datetime.today()-dateutil.relativedelta.relativedelta(months=-1))), '%Y-%m-%d')
+        enddate = datetime.strptime(request.GET.get('enddate',datetime.today()), '%Y-%m-%d')
         new_enddate = enddate + dateutil.relativedelta.relativedelta(days=1)
         vps = Vp.objects.filter(asset__assetOwner=request.user).order_by('vpNumber')
         medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaTimeStamp__range=[startdate,new_enddate]).order_by('-mediaTimeStamp').order_by('mediaVpNumber')
