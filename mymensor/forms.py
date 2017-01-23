f9abb4f81aa2b523d919e11e2ff75b4a78f25c1d
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-23 15:11:23 +0000

    23jan2017 14h57min

--- mymensor/forms.py
+++ mymensor/forms.py
@@ -8,15 +8,15 @@ class AssetForm(ModelForm):
     FREQ_UNIT_CHOICES = ('millis', 'hour', 'day', 'week', 'month')
 
     assetDescription = forms.CharField(max_length=1024)
-    assetNumber = forms.IntegerField(widget=forms.HiddenInput)
-    assetIsActive = forms.BooleanField(widget=forms.HiddenInput)
-    assetOwner = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput)  ###### FK
+    assetNumber = forms.IntegerField()#widget=forms.HiddenInput)
+    assetIsActive = forms.BooleanField()#widget=forms.HiddenInput)
+    assetOwner = forms.ModelChoiceField(queryset=User.objects.all())#, widget=forms.HiddenInput)  ###### FK
     assetOwnerDescription = forms.CharField(max_length=1024)
     assetOwnerKey = forms.CharField(max_length=1024)
     assetRegistryCode = forms.CharField(max_length=255)
     assetDciFrequencyUnit = forms.CharField() #widget=forms.Select(choices=FREQ_UNIT_CHOICES))
     assetDciFrequencyValue = forms.IntegerField()
-    assetDciQtyVps = forms.IntegerField(widget=forms.HiddenInput)
+    assetDciQtyVps = forms.IntegerField()#widget=forms.HiddenInput)
     assetDciTolerancePosition = forms.IntegerField()
     assetDciToleranceRotation = forms.IntegerField()
 
