013eba23f181b888674729c80645311701638861
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-23 20:54:35 +0000

    23jan2017 20h43min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -197,7 +197,7 @@ def vpSetupFormView(request):
     if request.method == 'GET':
         qtyvps = int(request.GET.get('qtyvps', 2))
         currentvp = int(request.GET.get('currentvp', 0))
-    vp = Vp.objects.filter(vpIsActive=True).filter(asset__assetOwner=request.user).filter(vpNumber=currentvp)[:1]
+    vp = Vp.objects.filter(vpIsActive=True)[:1]  #.filter(asset__assetOwner=request.user).filter(vpNumber=currentvp)
     form = VpForm(request.POST, instance=vp)
     if request.method == 'POST':
         if form.is_valid():
