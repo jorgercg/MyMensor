b25c75a9bc84709ee8a1bd9179e8155680ea1276
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-23 13:03:48 +0000

    23jan2017 11h41min

--- mymensor/forms.py
+++ mymensor/forms.py
@@ -1,8 +1,25 @@
 from django.db import models
 from django.forms import ModelForm
 from mymensor.models import Asset
+from django import forms
 
 class AssetForm(ModelForm):
+
+    FREQ_UNIT_CHOICES = ( 'millis', 'hour', 'day', 'week', 'month')
+
+    assetDescription = forms.CharField(max_length=1024, null=True)
+    assetNumber = forms.IntegerField(widget=forms.HiddenInput)
+    assetIsActive = forms.BooleanField(default=True)
+    assetOwner = forms.IntegerField(widget=forms.HiddenInput)  ###### FK
+    assetOwnerDescription = forms.CharField(max_length=1024, null=True)
+    assetOwnerKey = forms.CharField(max_length=1024, null=True)
+    assetRegistryCode = forms.CharField(max_length=255, null=True)
+    assetDciFrequencyUnit = forms.CharField(widget=forms.Select(choices=FREQ_UNIT_CHOICES), default="millis")
+    assetDciFrequencyValue = forms.IntegerField(default=20000)
+    assetDciQtyVps = forms.IntegerField(widget=forms.HiddenInput)
+    assetDciTolerancePosition = forms.IntegerField(default=50)
+    assetDciToleranceRotation = forms.IntegerField(default=10)
+
     class Meta:
         model = Asset
-        fields = ['assetOwnerDescription', 'assetOwnerKey', 'assetRegistryCode', 'assetDciFrequencyUnit', 'assetDciFrequencyValue', 'assetDciTolerancePosition', 'assetDciToleranceRotation']
+
