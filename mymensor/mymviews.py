bf27e5650c5b493647d6e8a2e138fffb9258b486
Jorge Goncalves <jrcgonc@gmail.com>
2017-01-24 14:59:58 +0000

    24jan2017 12h11min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -231,13 +231,14 @@ def tagSetupFormView(request):
         currentvp = int(request.GET.get('currentvp', 1))
         currenttag = int(request.GET.get('currenttag', 1))
         qtytags = int(request.GET.get('qtytags', qtytags))
+        if qtytags==0:
+            qtytags=1
         tag = Tag()
         try:
             tag = Tag.objects.filter(tagIsActive=True).filter(vp__asset__assetOwner=request.user).filter(vp__vpNumber=currentvp).filter(tagNumber=currenttag).get()
             form = TagForm(instance=tag)
         except tag.DoesNotExist:
-            tag = Tag(vp=Vp.objects.filter(vpIsActive=True).filter(asset__assetOwner=request.user).filter(
-                vpNumber=currentvp).get(), tagDescription='TAG#' + str(currenttag), tagNumber=currenttag,
+            tag = Tag(vp=Vp.objects.filter(vpIsActive=True).filter(asset__assetOwner=request.user).filter(vpNumber=currentvp).get(), tagDescription='TAG#'+str(currenttag), tagNumber=currenttag,
                          tagQuestion='Tag question for TAG#' + str(currenttag))
             form = TagForm(instance=tag)
 
