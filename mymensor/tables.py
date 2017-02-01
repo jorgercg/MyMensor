87b962be3853e0e88d1a7a21ded263a9708dfafd
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-01 21:27:50 +0000

    01fev2017 21h16min

--- mymensor/tables.py
+++ mymensor/tables.py
@@ -2,8 +2,8 @@ import django_tables2 as tables
 from mymensor.models import Tag, Vp, Media, ProcessedTag
 
 class TagStatusTable(tables.Table):
-    vpNumber = tables.Column(accessor='Vp.vpNumber')
-    vpDescription = tables.Column(accessor='Vp.vpDescription')
+    vpNumber = tables.Column(accessor='vp.vpNumber')
+    vpDescription = tables.Column(accessor='vp.vpDescription')
     class Meta:
         model = Tag
         attrs = {'class': 'paleblue'}
