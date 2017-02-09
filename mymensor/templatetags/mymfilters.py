da1dffc47a1b9bc50c97cef57550693603a76b4a
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-09 12:16:37 +0000

    09fev2017 11h33min

--- mymensor/templatetags/mymfilters.py
+++ mymensor/templatetags/mymfilters.py
@@ -13,3 +13,43 @@ def lookupvpnumber(value, key):
 @register.filter(name="frommillistoseconds")
 def frommillistoseconds(value):
     return int(value/1000)
+
+
+class GlobalVariable( object ):
+  def __init__( self, varname, varval ):
+    self.varname = varname
+    self.varval  = varval
+  def name( self ):
+    return self.varname
+  def value( self ):
+    return self.varval
+  def set( self, newval ):
+    self.varval = newval
+
+class GlobalVariableSetNode( template.Node ):
+  def __init__( self, varname, varval ):
+    self.varname = varname
+    self.varval  = varval
+  def render( self, context ):
+    gv = context.get( self.varname, None )
+    if gv:
+      gv.set( self.varval )
+    else:
+      gv = context[self.varname] = GlobalVariable( self.varname, self.varval )
+    return ''
+def setglobal( parser, token ):
+  try:
+    tag_name, varname, varval = token.contents.split(None, 2)
+  except ValueError:
+    raise template.TemplateSyntaxError("%r tag requires 2 arguments" % token.contents.split()[0])
+  return GlobalVariableSetNode( varname, varval )
+register.tag( 'setglobal', setglobal )
+
+class GlobalVariableGetNode( template.Node ):
+  def __init__( self, varname ):
+    self.varname = varname
+  def render( self, context ):
+    try:
+      return context[self.varname].value()
+    except AttributeError:
+      return ''
