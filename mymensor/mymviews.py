8e67198f5287d7222c8daf4bfa80c7bffc2c79f9
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-09 10:54:20 +0000

    09fev2017 10h52min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -329,7 +329,7 @@ def tagProcessingFormView(request):
                                     Params={'Bucket': AWS_S3_BUCKET_NAME,'Key': media.mediaObjectS3Key},
                                     ExpiresIn=3600)
         vpsofthemediasnotprocessedlist = medias.values_list('vp__id', flat=True)
-        vps = Vp.objects.annotate(qtyoftags=Count('Tag')).filter(asset__assetOwner=request.user).filter(vpIsActive=True).filter(id__in=vpsofthemediasnotprocessedlist).exclude(vpNumber=0).order_by('vpNumber').distinct()
+        vps = Vp.objects.annotate(qtyoftags=Count('tag')).filter(asset__assetOwner=request.user).filter(vpIsActive=True).filter(id__in=vpsofthemediasnotprocessedlist).exclude(vpNumber=0).order_by('vpNumber').distinct()
         tags = Tag.objects.filter(vp__asset__assetOwner=request.user).filter(tagIsActive=True).distinct()
         for vp in vps:
             vp.vpStdPhotoStorageURL = s3Client.generate_presigned_url('get_object',
