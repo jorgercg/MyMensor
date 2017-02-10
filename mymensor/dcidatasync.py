8d02291d32cad8760ebb8e6689f8df76e9acf0d6
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-10 13:16:59 +0000

    10fev2017 13h09min

--- mymensor/dcidatasync.py
+++ mymensor/dcidatasync.py
@@ -192,13 +192,13 @@ def createdcicfgbackup(request):
     session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                                     aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     s3Client = session.client('s3')
-    keys_to_backup = s3Client.list_objects_v2(Bucket=AWS_S3_BUCKET_NAME, Prefix=request.user.username, Delimiter='/')
+    keys_to_backup = s3Client.list_objects_v2(Bucket=AWS_S3_BUCKET_NAME, Prefix=request.user.username)
     s3 = boto3.resource('s3')
     bucket = s3.Bucket(AWS_S3_BUCKET_NAME)
-    for key_to_backup in keys_to_backup['Contents']['Key']:
+    for key_to_backup in keys_to_backup:
         replace = request.user.username
         withstring = request.user.username+"_backup"
-        newprefix,found,endpart = key_to_backup.partition(replace)
+        newprefix,found,endpart = key_to_backup['Contents']['Key'].partition(replace)
         newprefix+=withstring+endpart
         obj = bucket.Object(newprefix)
         obj.copy_from(AWS_S3_BUCKET_NAME+key_to_backup)
