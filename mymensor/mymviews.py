fb90a02553a507f01138b84cf0f608bfa4db0de2
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-13 11:58:37 +0000

    13fev2017 11h54min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -626,7 +626,7 @@ def vpDetailView(request):
         mediaselected = request.GET.get('mediaselected',0)
         medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(vp__vpNumber=vpselected).filter(mediaTimeStamp__range=[startdate, new_enddate]).order_by('mediaMillisSinceEpoch')
         mediaspks = medias.values_list('id', flat=True)
-        listofmediavpsnumbers = Vp.objects.filter(asset__vp__media__isnull=False).filter(asset__assetOwner=request.user).filter(mediaTimeStamp__range=[startdate, new_enddate]).filter(vpIsActive=True).order_by('vpNumber').distinct().values_list('vpNumber', flat=True)
+        listofmediavpsnumbers = Vp.objects.filter(asset__vp__media__isnull=False).filter(asset__assetOwner=request.user).filter(media__mediaTimeStamp__range=[startdate, new_enddate]).filter(vpIsActive=True).order_by('vpNumber').distinct().values_list('vpNumber', flat=True)
         if mediaselected == 0:
             mediaselected = medias.first().pk
         if mediaselected not in mediaspks:
