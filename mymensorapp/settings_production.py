728fa162ccb0faed736a0afed1d4830054090bcf
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-21 13:18:29 +0000

    21fev2017 10h43min

--- mymensorapp/settings_production.py
+++ mymensorapp/settings_production.py
@@ -19,3 +19,9 @@ DEBUG = True
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Ensure virtualenv path is part of PATH env var
+os.environ['PATH'] += os.pathsep + os.path.dirname(sys.executable)
+WKHTMLTOPDF_CMD = subprocess.Popen(
+    ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], # Note we default to 'wkhtmltopdf' as the binary name
+    stdout=subprocess.PIPE).communicate()[0].strip()
