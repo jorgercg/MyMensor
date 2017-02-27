c65192fd359d7f24237520c0a675f7c1b0987466
Jorge Goncalves <jrcgonc@gmail.com>
2017-02-27 13:01:27 +0000

    27fev2017 13h01min

--- mymensor/forms.py
+++ mymensor/forms.py
@@ -19,37 +19,6 @@ class VpForm(ModelForm):
 
 class TagForm(ModelForm):
 
-    vp = forms.ModelChoiceField(queryset=Vp.objects.all(), widget=forms.HiddenInput)  ###### FK
-    tagDescription = forms.CharField(max_length=1024)
-    tagNumber = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    tagIsActive = forms.BooleanField(initial=True)
-    tagListNumber = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    tagQuestion = forms.CharField(max_length=1024, required=False)
-    tagUnit = forms.CharField(max_length=50, required=False)
-    tagLowRed = forms.FloatField(required=False)
-    tagLowYellow = forms.FloatField(required=False)
-    tagExpValue = forms.FloatField(required=False)
-    tagHighYellow = forms.FloatField(required=False)
-    tagHighRed = forms.FloatField(required=False)
-    tagType = forms.CharField(max_length=50, widget=forms.HiddenInput, required=False)
-    tagIsDependantOfMasterTagNumber = forms.IntegerField(initial=0, widget=forms.HiddenInput, required=False)
-    tagMaxLagFromMasterTagInMillis = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    tagMaxLagFromSlaveTagsInMillis = forms.IntegerField(widget=forms.HiddenInput, required=False)
-    tagIsSetForSpecialCheck = forms.BooleanField(initial=False, widget=forms.HiddenInput, required=False)
-    tagSpecialCheckAcceptableDiscrepancy = forms.FloatField(initial=0, widget=forms.HiddenInput, required=False)
-
     class Meta:
         model = Tag
-        fields = '__all__'
-
-class ValueForm(ModelForm):
-    processedTag = forms.ModelChoiceField(queryset=Tag.objects.all(), widget=forms.HiddenInput)  ###### FK
-    processorUserId = forms.ModelChoiceField(queryset=User.objects.all(), widget=forms.HiddenInput)  ###### FK
-    valValue = forms.FloatField()
-    valValueEntryDBTimeStamp = forms.DateTimeField(widget=forms.HiddenInput)
-    valEvalStatus = forms.CharField(widget=forms.HiddenInput, required=False)
-    tagStateResultingFromValValueStatus = forms.IntegerField(widget=forms.HiddenInput, required=False)
-
-    class Meta:
-        model = Value
-        fields = '__all__'
+        fields = ['tagDescription', 'tagIsActive', 'tagQuestion', 'tagUnit', 'tagLowRed', 'tagLowYellow', 'tagExpValue', 'tagHighYellow', 'tagHighRed']
