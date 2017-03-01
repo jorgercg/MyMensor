2338a1104372121ae8405b04956be968d5eb099e
Jorge Goncalves <jrcgonc@gmail.com>
2017-03-01 17:32:08 +0000

    01mar2017 17h32min

--- mymensor/signals.py
+++ mymensor/signals.py
@@ -6,31 +6,32 @@ from mymensor.mymfunctions import setup_new_user
 from mymensorapp.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME
 import boto3
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
         setup_new_user(instance)
 
+
 @receiver(pre_delete, sender=settings.AUTH_USER_MODEL)
 def delete_cognito_related_id(sender, instance=None, **kwargs):
-    if instance:
-        client = boto3.client(
-            'cognito-identity',
-            'eu-west-1',
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    client = boto3.client(
+        'cognito-identity',
+        'eu-west-1',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+
+    assetinstance = Asset.objects.get(assetOwner=instance)
+
+    usercogidtodelete = assetinstance.assetOwnerIdentityId
+
+    try:
+        response = client.delete_identities(
+            IdentityIdsToDelete=[
+                usercogidtodelete,
+            ]
         )
-
-        assetinstance = Asset.objects.get(assetOwner=instance)
-
-        usercogidtodelete = assetinstance.assetOwnerIdentityId
-
-        try:
-            response = client.delete_identities(
-                IdentityIdsToDelete=[
-                    usercogidtodelete,
-                ]
-            )
-        except:
-            pass
+    except:
+        pass
