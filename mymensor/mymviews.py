5d5b3b30e7ccdac89f327a6791f0fc25454da93b
Jorge Goncalves <jrcgonc@gmail.com>
2017-03-01 14:19:17 +0000

    01mar2017 14h15min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -163,7 +163,7 @@ def portfolio(request):
         qtypervp = int(request.GET.get('qtypervp', 5))
         medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(vp__vpIsActive=True).filter(
             mediaTimeStamp__range=[startdate, new_enddate]).order_by('-mediaMillisSinceEpoch')
-        vps = Vp.objects.filter(asset__assetOwner=request.user).filter(vpIsActive=True).filter(asset__vp__media__isnull=False).filter(
+        vps = Vp.objects.filter(asset__assetOwner=request.user).filter(vpIsActive=True).filter(
                     media__mediaTimeStamp__range=[startdate, new_enddate]).order_by('vpNumber')
         startdateformatted = startdate.strftime('%Y-%m-%d')
         enddateformatted = enddate.strftime('%Y-%m-%d')
