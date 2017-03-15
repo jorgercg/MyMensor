668bf5e41f328d7072926b61d5e968e473a52295
Jorge Goncalves <jrcgonc@gmail.com>
2017-03-15 17:46:01 +0000

    15mar2017 17h45min

--- mymensor/templatetags/mymfilters.py
+++ mymensor/templatetags/mymfilters.py
@@ -1,5 +1,8 @@
 import re
+import urllib
 from django.utils.safestring import mark_safe
+from django.template.defaultfilters import stringfilter
+from django.utils.html import conditional_escape
 from django import template
 
 register = template.Library()
@@ -33,6 +36,27 @@ def add_class(value, css_class):
     else:
         return mark_safe(string.replace('>', ' class="%s">' % css_class))
     return value
+
+
+@register.filter
+@stringfilter
+def qrcode(value, alt=None):
+    """
+    Generate QR Code image from a string with the Google charts API
+
+    http://code.google.com/intl/fr-FR/apis/chart/types.html#qrcodes
+
+    Exemple usage --
+    {{ my_string|qrcode:"my alt" }}
+
+    <img src="http://chart.apis.google.com/chart?chs=150x150&amp;cht=qr&amp;chl=my_string&amp;choe=UTF-8" alt="my alt" />
+    """
+
+    url = conditional_escape("http://chart.apis.google.com/chart?%s" % \
+                             urllib.urlencode({'chs': '150x150', 'cht': 'qr', 'chl': value, 'choe': 'UTF-8'}))
+    alt = conditional_escape(alt or value)
+
+    return mark_safe(u"""<img class="qrcode" src="%s" width="150" height="150" alt="%s" />""" % (url, alt))
 
 
 
