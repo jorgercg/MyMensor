7b83b4be24886f30b7f120c872170503e55986e3
Jorge Goncalves <jrcgonc@gmail.com>
2017-04-02 12:07:12 +0100

    02abr2017 12h06min

--- mymensor/templates/js/handler.js
+++ mymensor/templates/js/handler.js
@@ -2,25 +2,15 @@
 
 function handlers_for_event(event_class, channel, message, data, site, uid) {
     if (( event_class == 'NewMedia' ) && ("{{user.get_username}}" == JSON.stringify(data.username).replace(/\"/g, ""))) {
-        console.log("NewMedia");
-        console.log("Event Class:" + event_class);
-        console.log("Channel:" + channel);
-        console.log("Message:" + message);
-        console.log("Data:" + data);
-        console.log("Site:" + site);
-        console.log("Uid:" + uid);
+        location.reload(true);
+        //console.log("NewMedia");
+        //console.log("Event Class:" + event_class);
+        //console.log("Channel:" + channel);
+        //console.log("Message:" + message);
+        //console.log("Data:" + data);
+        //console.log("Site:" + site);
+        //console.log("Uid:" + uid);
     }
-
-    if ( event_class == 'NewMedia' )  {
-        console.log("NewMedia");
-        console.log("Event Class:" + event_class);
-        console.log("Channel:" + channel);
-        console.log("Message:" + message);
-        console.log("Data:" + data);
-        console.log("Site:" + site);
-        console.log("Uid:" + uid);
-    }
-
 }
 
 
