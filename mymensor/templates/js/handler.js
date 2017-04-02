f3557a6edeef3c08602e581d0752884c0c9f5e65
Jorge Goncalves <jrcgonc@gmail.com>
2017-04-02 11:39:04 +0100

    02abr2017 11h38min

--- mymensor/templates/js/handler.js
+++ mymensor/templates/js/handler.js
@@ -11,6 +11,16 @@ function handlers_for_event(event_class, channel, message, data, site, uid) {
         console.log("Uid:" + uid);
     }
 
+    if ( event_class == 'NewMedia' )  {
+        console.log("NewMedia");
+        console.log("Event Class:" + event_class);
+        console.log("Channel:" + channel);
+        console.log("Message:" + message);
+        console.log("Data:" + data);
+        console.log("Site:" + site);
+        console.log("Uid:" + uid);
+    }
+
 }
 
 
