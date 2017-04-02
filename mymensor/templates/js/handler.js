a7ae6dba98146b33b69b03b7dc31765c94921387
Jorge Goncalves <jrcgonc@gmail.com>
2017-04-02 11:22:03 +0100

    02abr2017 11h21min

--- mymensor/templates/js/handler.js
+++ mymensor/templates/js/handler.js
@@ -1,3 +1,11 @@
-if ( event_class == 'NewMedia' ) {
+{% load instant_tags %}
+
+function handlers_for_event(event_class, channel, message, data, site, uid) {
+	    if ( event_class == 'NewMedia' ) {
         console.log("NewMedia");
+        }
 }
+
+
+
+
