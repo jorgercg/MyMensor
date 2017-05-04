8397b27403a922d79aef67d7fea2ceea3bb3d1d1
Jorge Goncalves <jrcgonc@gmail.com>
2017-05-04 11:02:40 +0100

    04mai2017 11h02min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -45,6 +45,7 @@ def landingView(request):
             session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY_ID,
                                             aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
             s3 = session.resource('s3')
+            mediaObjectS3KeyEncoded = urllib.quote(mediaObjectS3Key)
             object = s3.Object(AWS_S3_BUCKET_NAME, mediaObjectS3Key)
             object.load()
             obj_metadata = object.metadata
