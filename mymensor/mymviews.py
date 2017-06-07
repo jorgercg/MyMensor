632a6eba13856949d630b06eb334df4ebe91b74a
Jorge Goncalves <jrcgonc@gmail.com>
2017-06-07 16:06:33 +0100

    07jun2017 16h06min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -519,6 +519,7 @@ def tagSetupFormView(request):
         tags = Tag.objects.filter(vp__asset__assetOwner=request.user).filter(vp__vpNumber=currentvp).order_by('tagNumber')
     else:
         tags = None
+        lasttag = None
 
     vps = Vp.objects.filter(asset__assetOwner=request.user).filter(vpIsActive=True).exclude(vpNumber=0).order_by(
         'vpNumber')
