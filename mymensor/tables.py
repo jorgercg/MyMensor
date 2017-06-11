2c1ecebd1d82b3532143cdc000ff7bf881e2bcbc
Jorge Goncalves <jrcgonc@gmail.com>
2017-06-11 17:55:50 +0100

    11jun2017 17h55min

--- mymensor/tables.py
+++ mymensor/tables.py
@@ -3,10 +3,21 @@ from django.utils.translation import ugettext_lazy as _
 from mymensor.models import TagStatusTable
 
 class TagStatusTableClass(tables.Table):
+    TAG_STATUS_CHOICES = (
+    ('NP', _('NOT PROCESSED')), ('PR', _('PROCESSED')), ('LR', 'LOW RED'), ('LY', 'LOW YELLOW'), ('GR', 'GREEN'),
+    ('HY', 'HIGH YELLOW'), ('HR', 'HIGH RED'),)
+
+    statusTagNumber = tables.Column(verbose_name=_('Tag#'))
+    statusTagDescription = tables.Column(verbose_name=_('Tag Description'))
+    statusVpNumber = tables.Column(verbose_name=_('VP#'))
+    statusVpDescription = tables.Column(verbose_name=_('VP Description'))
+    statusValValueEvaluated = tables.Column(verbose_name=_('Value'))
+    statusTagUnit = tables.Column(verbose_name=_('Unit'))
+    statusMediaTimeStamp = tables.Column(verbose_name=_('Media Time'))
+    statusDBTimeStamp = tables.Column(verbose_name=_('Processing Time'))
+    statusTagStateEvaluated = tables.Column(verbose_name=_('Status'))
+
     class Meta:
-        TAG_STATUS_CHOICES = (
-        ('NP', _('NOT PROCESSED')), ('PR', _('PROCESSED')), ('LR', 'LOW RED'), ('LY', 'LOW YELLOW'), ('GR', 'GREEN'),
-        ('HY', 'HIGH YELLOW'), ('HR', 'HIGH RED'),)
         model = TagStatusTable
         # add class="paleblue" to <table> tag
         attrs = {'class': 'table-sm table-striped table-bordered table-hover table-responsive mym-table'}
