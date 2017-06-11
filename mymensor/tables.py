06058c9d587ba4571dae6199e35a5bef0f94c95d
Jorge Goncalves <jrcgonc@gmail.com>
2017-06-11 17:30:42 +0100

    11jun2017 17h30min

--- mymensor/tables.py
+++ mymensor/tables.py
@@ -1,8 +1,12 @@
 import django_tables2 as tables
+from django.utils.translation import ugettext_lazy as _
 from mymensor.models import TagStatusTable
 
 class TagStatusTableClass(tables.Table):
     class Meta:
+        TAG_STATUS_CHOICES = (
+        ('NP', _('NOT PROCESSED')), ('PR', _('PROCESSED')), ('LR', 'LOW RED'), ('LY', 'LOW YELLOW'), ('GR', 'GREEN'),
+        ('HY', 'HIGH YELLOW'), ('HR', 'HIGH RED'),)
         model = TagStatusTable
         # add class="paleblue" to <table> tag
         attrs = {'class': 'table-sm table-striped table-bordered table-hover table-responsive mym-table'}
