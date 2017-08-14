61e98a994da14bce2b16ec46dcb3275098fef042
Jorge Goncalves <jrcgonc@gmail.com>
2017-08-14 19:40:36 +0100

    14ago2017 19h40min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -457,7 +457,7 @@ def location(request):
         showuserpath = int(request.GET.get('showuserpath', 0))
         centerlat = float(request.GET.get('centerlat', 0))
         centerlng = float(request.GET.get('centerlng', 0))
-        mapzoom = int(request.GET.get('centerlat', 0))
+        mapzoom = int(request.GET.get('mapzoom', 0))
         vps = Vp.objects.filter(asset__assetOwner=request.user).filter(vpIsActive=True).order_by('vpNumber')
         vpslist = vps
         vpsselectedfromlist = vps.values_list('vpNumber', flat=True)
