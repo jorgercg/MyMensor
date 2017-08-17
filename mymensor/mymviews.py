9304dde0cd82e64a8603fd9ec8ba810c69b6958e
Jorge Goncalves <jrcgonc@gmail.com>
2017-08-17 21:19:04 +0100

    17ago2017 21h18min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -469,7 +469,7 @@ def location(request):
         centerlng = float(request.GET.get('centerlng', 0))
         mapzoom = int(request.GET.get('mapzoom', 0))
         vps = Vp.objects.filter(asset__assetOwner=request.user).filter(asset__vp__media__isnull=False).filter(
-            media__mediaTimeStamp__range=[startdate, new_enddate]).filter(vpIsActive=True).order_by(
+            media__mediaTimeStamp__range=[startdate, new_enddate]).filter(vpIsActive=True).annotate(num_media=Count('media')).order_by(
             'vpNumber').distinct()
         vpslist = vps
         vpsselectedfromlist = vps.values_list('vpNumber', flat=True)
