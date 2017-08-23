2868aaf578803f3dea5bc21d79565f60a12cd5eb
Jorge Goncalves <jrcgonc@gmail.com>
2017-08-23 09:42:51 +0100

    23ago2017 09h42min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -1756,9 +1756,22 @@ def subscription(request):
         mediaqty = Media.objects.filter(vp__asset__assetOwner=request.user).count()
         tagqty = Tag.objects.filter(vp__asset__assetOwner=request.user).count()
         processedtagqty = ProcessedTag.objects.filter(tag__vp__asset__assetOwner=request.user).count()
+
+        swalchgbtntitle = _('Really Change Plan?')
+        swalchgbtnconfirmButtonText = _('Confirm')
+        swalchgbtncancelButtonText = _('Cancel')
+        swalchgbtnsuccesstitle = _("Done!")
+        swalchgbtncanceltitle = _("No change!")
+
+
+
+
         return render(request, 'subscription.html', {'userloggedin': request.user, 'btcustomer': btcustomer,
                                                      'btsubscription': btsubscription, 'currentuserplan': currentuserplan,
                                                      'dateofendoftrialbeforesubscription': dateofendoftrialbeforesubscription,
+                                                     'swalchgbtntitle':swalchgbtntitle,'swalchgbtnconfirmButtonText':swalchgbtnconfirmButtonText,
+                                                     'swalchgbtncancelButtonText': swalchgbtncancelButtonText,
+                                                     'swalchgbtnsuccesstitle': swalchgbtnsuccesstitle, 'swalchgbtncanceltitle': swalchgbtncanceltitle,
                                                      'mediaqty':mediaqty, 'tagqty':tagqty ,'processedtagqty':processedtagqty})
     return HttpResponse(status=404)
 
