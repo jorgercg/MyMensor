0dfc75ff707df7ed628b31c663ed36ca7511a8b5
Jorge Goncalves <jrcgonc@gmail.com>
2017-09-06 12:59:05 +0100

    06set2017 12h58min

--- mymensor/mymviews.py
+++ mymensor/mymviews.py
@@ -418,9 +418,10 @@ def portfolio(request):
             if showlastmedia == 1:
                 lastmedia = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaLocIsCertified=True).filter(
                     mediaTimeIsCertified=True).filter(vp__vpNumber__in=vpsselected).order_by('-mediaMillisSinceEpoch').first()
-                if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
-                    new_enddate = lastmedia.mediaTimeStamp
-                    enddate = lastmedia.mediaTimeStamp
+                if lastmedia:
+                    if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
+                        new_enddate = lastmedia.mediaTimeStamp
+                        enddate = lastmedia.mediaTimeStamp
             medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaLocIsCertified=True).filter(
                 mediaTimeIsCertified=True).filter(vp__vpNumber__in=vpsselected).filter(
                 mediaTimeStamp__range=[startdate, new_enddate]).order_by('-mediaMillisSinceEpoch')
@@ -428,9 +429,10 @@ def portfolio(request):
             if showlastmedia == 1:
                 lastmedia = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaLocIsCertified=True).filter(
                     vp__vpNumber__in=vpsselected).order_by('-mediaMillisSinceEpoch').first()
-                if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
-                    new_enddate = lastmedia.mediaTimeStamp
-                    enddate = lastmedia.mediaTimeStamp
+                if lastmedia:
+                    if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
+                        new_enddate = lastmedia.mediaTimeStamp
+                        enddate = lastmedia.mediaTimeStamp
             medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(mediaLocIsCertified=True).filter(
                 vp__vpNumber__in=vpsselected).filter(
                 mediaTimeStamp__range=[startdate, new_enddate]).order_by('-mediaMillisSinceEpoch')
@@ -438,9 +440,10 @@ def portfolio(request):
             if showlastmedia == 1:
                 lastmedia = Media.objects.filter(vp__asset__assetOwner=request.user).filter(
                     mediaTimeIsCertified=True).filter(vp__vpNumber__in=vpsselected).order_by('-mediaMillisSinceEpoch').first()
-                if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
-                    new_enddate = lastmedia.mediaTimeStamp
-                    enddate = lastmedia.mediaTimeStamp
+                if lastmedia:
+                    if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
+                        new_enddate = lastmedia.mediaTimeStamp
+                        enddate = lastmedia.mediaTimeStamp
             medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(
                 mediaTimeIsCertified=True).filter(vp__vpNumber__in=vpsselected).filter(
                 mediaTimeStamp__range=[startdate, new_enddate]).order_by('-mediaMillisSinceEpoch')
@@ -448,9 +451,10 @@ def portfolio(request):
             if showlastmedia == 1:
                 lastmedia = Media.objects.filter(vp__asset__assetOwner=request.user).filter(
                     vp__vpNumber__in=vpsselected).order_by('-mediaMillisSinceEpoch').first()
-                if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
-                    new_enddate = lastmedia.mediaTimeStamp
-                    enddate = lastmedia.mediaTimeStamp
+                if lastmedia:
+                    if (lastmedia.mediaTimeStamp - new_enddate).total_seconds() > 0:
+                        new_enddate = lastmedia.mediaTimeStamp
+                        enddate = lastmedia.mediaTimeStamp
             medias = Media.objects.filter(vp__asset__assetOwner=request.user).filter(
                 vp__vpNumber__in=vpsselected).filter(
                 mediaTimeStamp__range=[startdate, new_enddate]).order_by('-mediaMillisSinceEpoch')
