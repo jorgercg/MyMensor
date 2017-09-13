5e4305ec61848e78ff27d954b223a44cf73123ab
Jorge Goncalves <jrcgonc@gmail.com>
2017-09-13 14:00:55 +0100

    13set2017 14h00min

--- mymensorapp/settings_production.py
+++ mymensorapp/settings_production.py
@@ -14,7 +14,7 @@ CSRF_COOKIE_SECURE = True
 # UPDATE BEFORE LAUNCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 ALLOWED_HOSTS = ['app.mymensor.com']
 
-DEBUG = False
+DEBUG = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
