2c82c42e1db90cc09894467c0fb15ccdeb09986d
Jorge Goncalves <jrcgonc@gmail.com>
2017-12-06 12:38:11 +0000

    06dez2017 12h38

--- mymensorapp/wsgi.py
+++ mymensorapp/wsgi.py
@@ -13,7 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mymensorapp.settings_production
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
